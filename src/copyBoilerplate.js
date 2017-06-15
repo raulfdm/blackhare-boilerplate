9e0a1ab02967d8535ae680926c7719c552815e62
Raul F. de Melo <melo.raulf@gmail.com>
2017-06-15 08:42:35 +0100

    Now package.json is create dynamically based on user answers

--- src/copyBoilerplate.js
+++ src/copyBoilerplate.js
@@ -1,12 +1,18 @@
 const fs = require('fs-extra')
+const packageJson = require('./files/package.json')
 
 const copyBoilerplate = projectInfos => {
     const mirrorPath = `${__dirname}/mirror`
     const projectName = projectInfos.project.name
-    
+
     const newFolderPath = `${process.cwd()}/${projectName}`
-    
-    return fs.copy(mirrorPath,newFolderPath)
+
+    return fs
+        .copy(mirrorPath, newFolderPath)
+        .then(() => {
+            packageJson(projectInfos.project, newFolderPath)
+        })
+
 }
 
 module.exports = copyBoilerplate
