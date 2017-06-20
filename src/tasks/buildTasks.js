9820ede15446f32d1f28835e9e56116584031001
Raul F. de Melo <melo.raulf@gmail.com>
2017-06-20 18:09:33 +0100

    Removed unused variable

--- src/tasks/buildTasks.js
+++ src/tasks/buildTasks.js
@@ -2,33 +2,33 @@ const exec = require('child_process').exec
 const Spinner = require('../helpers/spinners')
 
 const installDependencies = projectInfos => {
-	const spinn = new Spinner({
-		startMessage: 'Instaling dependencies...',
-		successMessage: 'Dependencies installed successfully...',
-		failureMessage: 'Error to Install Dependencies'
-	})
+  const spinn = new Spinner({
+    startMessage: 'Instaling dependencies...',
+    successMessage: 'Dependencies installed successfully...',
+    failureMessage: 'Error to Install Dependencies'
+  })
 
-	const commands = {
-		downFolder: `cd ${projectInfos.name}`,
-		install: `${projectInfos.package_manager} install`,
-	}
+  const commands = {
+    downFolder: `cd ${projectInfos.name}`,
+    install: `${projectInfos.package_manager} install`,
+  }
 
-	return new Promise((resolve, reject) => {
-		spinn.start()
+  return new Promise((resolve, reject) => {
+    spinn.start()
 
-		exec(commands.downFolder + ' && ' + commands.install, (err, stdout, stderr) => {
-			if (err) {
-				spinn.fail()
-				reject(err)
-			} else {
-				spinn.done()
-				resolve(stdout)
-			}
+    exec(commands.downFolder + ' && ' + commands.install, (err, stdout) => {
+      if (err) {
+        spinn.fail()
+        reject(err)
+      } else {
+        spinn.done()
+        resolve(stdout)
+      }
 
-		})
-	})
+    })
+  })
 }
 
 module.exports = {
-	installDependencies
+  installDependencies
 }
