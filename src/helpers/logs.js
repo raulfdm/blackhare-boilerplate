2362ad1424292bea2704db1c1f7df2a2a269d3d5
Raul F. de Melo <melo.raulf@gmail.com>
2017-06-20 18:10:28 +0100

    Added trace console, added and changed some log styles

--- src/helpers/logs.js
+++ src/helpers/logs.js
@@ -1,18 +1,26 @@
 const chalk = require('chalk')
 const cLog = console.log
 const cErr = console.error
+const ctrace = console.trace
 
 /*Chalk Styles*/
-const errorStyle = chalk.bold.red
-const sucessStyle = chalk.bold.green
-const infoStyle = chalk.bold.blue
+const errorStyle = chalk.red
+const sucessStyle = chalk.green
+const infoStyle = chalk.yellow
+const boldStyle = chalk.bold
 
 const error = message => cErr(errorStyle(message))
+const errorTrace = message => ctrace(message)
 const success = message => cLog(sucessStyle(message))
 const info = message => cLog(infoStyle(message))
+const log = message => cLog(message)
+const bold = message => cLog(boldStyle(message))
 
 module.exports = {
-	error,
-	success,
-	info
+  error,
+  success,
+  info,
+  log,
+  errorTrace,
+  bold
 }
